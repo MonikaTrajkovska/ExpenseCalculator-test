6650722147937cc2d69323f9b75bf740fa7b5a04
Monika <moni_bonboni90@hotmail.com>
2019-12-21 13:16:40 +0100

    finnished additem

--- client/src/components/NewProduct.js
+++ client/src/components/NewProduct.js
@@ -4,34 +4,37 @@ import './New product.css'
 import {connect} from 'react-redux'
 import {addItem} from '../actions/itemActions'
 import uuid from 'uuid'
-
+import {Link} from 'react-router-dom'
 
 class NewProduct extends React.Component{
-    state={
-        modal:false,
-        product_name:'',
-        product_type:'',
-        product_description:'',
-        purchase_date:'',
-        product_price:''
-    }
-    toggle=()=>{
-        this.setState({
-            modal:!this.state.modal
-        })
-    }
-//      onChange = e => {
-//     this.setState({ [e.target.name]: e.target.value });
-//   };
+constructor(props){
+    super(props)
+      this.state={
+            modal:false,
+            product_name:'',
+            product_type:'',
+            product_description:'',
+            purchase_date:'',
+            product_price:''
+        }
+    
+}
+toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+  
+    onChange = e => {     this.setState({ [e.target.id]: e.target.value });   };
     onSubmit=e=>{
         e.preventDefault()
         const newItem={
             id:uuid(),
-            product_name:this.state,
-            product_type:this.state,
-        product_description:this.state,
-        purchase_date:this.state,
-        product_price:this.state
+            product_name:this.state.product_name,
+            product_type:this.state.product_type,
+        product_description:this.state.product_description,
+        purchase_date:this.state.purchase_date,
+        product_price:this.state.product_price
         }
         this.props.addItem(newItem)
         this.toggle()
@@ -40,38 +43,38 @@ class NewProduct extends React.Component{
         return(
            
                   <React.Fragment>
-                
-       <h3 >New Product</h3>
+          
+           <h3 >New Product</h3>
       
        <div id="login2">
             <div className="box-container2">
                 <form onSubmit={this.onSubmit}>
                      <p className="input-container2">
                          <label className="text-field-input2" >Product name</label>
-                         <input type="text"  product_name="product_name" id="product_name" onChange={this.onChange} className="text-field2" />
+                         <input type="text"   id="product_name" defaultValue={this.state.product_name} onChange={this.onChange} className="text-field2" />
                      </p>
-                     <p className="input-container2">
-                         <label className="text-field-input2" >Product description</label>
-                         <input type="text" product_description="product_description" id="product_description" onChange={this.onChange} className="text-field2"  />
-                     </p>
+                    
                      <p className="input-container2">
                          <label className="text-field-input2" >Product type</label>                        
-                          <input type="text" product_type="product_type" id="product_type" onChange={this.onChange} className="text-field2" />
+                          <input type="text"  id="product_type" defaultValue={this.state.product_type} onChange={this.onChange} className="text-field2" />
                     </p>
+                    <p className="input-container2">
+                         <label className="text-field-input2" >Product description</label>
+                         <input type="text"  id="product_description" defaultValue={this.state.product_description} onChange={this.onChange} className="text-field2"  />
+                     </p>
                      <p className="input-container2">
                         <label className="text-field-input2"  >Purchase date</label>
-                        <input type="text" purchase_date="purchase_date" id="purchase_date" onChange={this.onChange} className="text-field2" />
+                        <input type="text"  id="purchase_date" defaultValue={this.state.purchase_date}  onChange={this.onChange} className="text-field2" />
                     </p>
                     <p className="input-container2">
                         <label className="text-field-input2"  >Product price</label>
-                         <input type="number" product_price="product_price" id="product_price" onChange={this.onChange} className="text-field2" />
+                         <input type="number" id="product_price" defaultValue={this.state.product_price} onChange={this.onChange} className="text-field2" />
                     </p>
-                    <div>
-                <button className="primary-button2" onClick={this.toggle}>Create product</button> 
-            </div>
-        
-                    
-                     </form>
+                    <Link to="/products">
+                    <button className="primary-button2"
+               onClick= {this.onSubmit}  >Create product</button> 
+               </Link>
+               </form>
                      </div>
                     
                     <div className="new2"> 
@@ -79,7 +82,7 @@ class NewProduct extends React.Component{
                         <p>You are creating new product</p>
                    </div>
                 </div>
-                    
+              
                 </React.Fragment>
                            
                     
